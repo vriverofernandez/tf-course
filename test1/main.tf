34c59249e7920e694f9ce175973757660b24c272
Víctor Rivero Fernández <vriverofernandez@gmail.com>
2023-10-09 18:01:07 +0200

    update main

--- test1/main.tf
+++ test1/main.tf
@@ -11,7 +11,23 @@ resource "azurerm_resource_group" "resource_group" {
 }
 */
 
-resource "azurerm_resource_group" "vnet" {
-  name     = "tf-test-victor"
-  location = "West Europe"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-test-victor"
+  location            = "West Europe"
+  resource_group_name = "test-tf"
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
