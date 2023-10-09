fa95acc13b41433a1bee443ebf0d964190212b69
Víctor Rivero Fernández <vriverofernandez@gmail.com>
2023-10-09 17:47:09 +0200

    update main

--- test1/main.tf
+++ test1/main.tf
@@ -1,9 +1,17 @@
+
+
 provider "azurerm" {
   features {}
 }
-
+/*
 resource "azurerm_resource_group" "resource_group" {
-  name     = "test-course-tf-rg"
+  name     = "otro"
   location = "West Europe"
-  tags     = { env = "dev", pay_account = "project 1" }
+  tags     = { env = "dev", pay_account = "project 1", sadf="asdf" }
+}
+*/
+
+resource "azurerm_resource_group" "vnet" {
+  name     = "tf-test-victor"
+  location = "West Europe"
 }
