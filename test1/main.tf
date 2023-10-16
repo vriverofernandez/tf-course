9d375352aecc903c26d31d2e0d49d9b361489b38
Víctor Rivero Fernández <vriverofernandez@gmail.com>
2023-10-16 17:14:31 +0200

    mas cambios

--- test1/main.tf
+++ test1/main.tf
@@ -1,25 +1,23 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = var.name
   location            = var.location
-  resource_group_name = "test-tf"
-  address_space       = ["10.0.0.0/16"]
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
 
   subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
+    name           = var.subnet_name[0]
+    address_prefix = var.subnet_address_prefix[0]
   }
 
   subnet {
-    name           = "subnet3"
-    address_prefix = "10.0.3.0/24"
+    name           = var.subnet_name[1]
+    address_prefix = var.subnet_address_prefix[1]
   }
 
-  tags = {
-    environment = "Production"
+  subnet {
+    name           = var.subnet_name[2]
+    address_prefix = var.subnet_address_prefix[2]
   }
+
+  tags = var.tags
 }
