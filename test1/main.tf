ea2582beca3052cf693d769b3e2b3cb9715baff0
JOSE ENRIQUE GARCES BAHAMONDE <0012853@viewnext.com>
2023-10-16 18:30:16 +0200

    nuevo commit

--- test1/main.tf
+++ test1/main.tf
@@ -1,23 +1,35 @@
+resource "azurerm_network_security_group" "nsg_prueba" {
+  name = "ejemplo_nsg"
+  location = var.location
+  resource_group_name = var.resource_group_name
+
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = var.address_space
+  
 
   subnet {
     name           = var.subnet_name[0]
     address_prefix = var.subnet_address_prefix[0]
+    security_group = azurerm_network_security_group.nsg_prueba.id
   }
 
   subnet {
     name           = var.subnet_name[1]
     address_prefix = var.subnet_address_prefix[1]
+    security_group = azurerm_network_security_group.nsg_prueba.id
   }
 
   subnet {
     name           = var.subnet_name[2]
     address_prefix = var.subnet_address_prefix[2]
+    security_group = azurerm_network_security_group.nsg_prueba.id
   }
 
   tags = var.tags
 }
+
--- test1/pre.tfvars
+++ test1/pre.tfvars
@@ -1,7 +1,7 @@
-name = "vn-pre"
+name = "vn-pre_sg"
 location = "west europe"
 resource_group_name = "test-tf"
 address_space = ["10.0.0.0/16"]
-tags = ["environment=preproduction" ]
+tags = {"environment":"preproduction"}
 subnet_name = [ "subnet11", "subnet12", "subnet13" ]
 subnet_address_prefix = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
